998dc7c16c86ad6f71d6e5f12f8b66856c14eb53
eliezereoc <eliezeroc@gmail.com>
2021-06-30 08:15:24 -0300

    Exemplo bidings

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  propLegal = 'Qaulquer';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo...');
+    
   }
 
 }
